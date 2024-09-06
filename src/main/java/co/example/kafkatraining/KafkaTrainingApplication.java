c9c47cbab698b99de38459923936571dfa018dbb
Andres Zapata <afzar27@gmail.com>
2024-09-06 16:08:26 -0500

    INIT COMMIT

---
+++ src/main/java/co/example/kafkatraining/KafkaTrainingApplication.java
@@ -0,0 +1,13 @@
+package co.example.kafkatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTrainingApplication.class, args);
+    }
+
+}
